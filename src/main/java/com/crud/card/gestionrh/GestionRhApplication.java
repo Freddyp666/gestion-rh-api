b99852be520858a39a3ca4f4dfb013c9930e3b81
Freddyp666 <pat-.-freddy@outlook.com>
2026-02-18 22:41:43 -0500

    Initial commit

---
+++ src/main/java/com/crud/card/gestionrh/GestionRhApplication.java
@@ -0,0 +1,16 @@
+package com.crud.card.gestionrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRhApplication.class, args);
+
+        System.out.println("Holala");
+    }
+
+}
+
---
+++ src/test/java/com/crud/card/gestionrh/GestionRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.card.gestionrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
